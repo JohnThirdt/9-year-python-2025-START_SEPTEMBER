83e9175dd5c389a201735fff6f03271e6e179308
JohnThirdt <lvantretiyakov2018@gmail.com>
2025-11-21 21:04:42 +0300

    Сделано 000 задание - первая попытка

---
+++ lessons/7/1.py
@@ -0,0 +1,19 @@
+password = 123456789
+
+N = [32, 127]
+symbols = [chr(i) for i in range(N[0], N[1])]
+
+# def gen_password(i, n):
+
+
+
+
+
+str = ''
+n = 0
+while n != password:
+    n += 1
+
+
+
+print(f"{n} is the password!?")
